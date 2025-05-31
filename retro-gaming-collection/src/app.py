a9e4117d2993b079ecf97f58cf92192b9060bc75
graememccall <graememccall@hotmail.com>
2025-05-31 20:42:13 +0100

    Another commit

--- retro-gaming-collection/src/app.py
+++ retro-gaming-collection/src/app.py
@@ -1,14 +1,44 @@
-from flask import Flask
-from db import init_db
-from routes import register_routes
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Console
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-# Initialize the database
-init_db()
+# Switch database based on environment
+if os.getenv("FLASK_ENV") == "production":
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 
-# Register routes
-register_routes(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return redirect(url_for('view_consoles'))
+
+@app.route('/consoles')
+def view_consoles():
+    consoles = Console.query.all()
+    return render_template('view_consoles.html', consoles=consoles)
+
+@app.route('/consoles/add', methods=['GET', 'POST'])
+def add_console():
+    if request.method == 'POST':
+        name = request.form['name']
+        manufacturer = request.form['manufacturer']
+        release_year = request.form['release_year']
+        new_console = Console(name=name, manufacturer=manufacturer, release_year=release_year)
+        db.session.add(new_console)
+        db.session.commit()
+        return redirect(url_for('view_consoles'))
+    return render_template('add_console.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- retro-gaming-collection/src/models.py
+++ retro-gaming-collection/src/models.py
@@ -1,26 +1,9 @@
-class Game:
-    def __init__(self, id, title, genre, release_year):
-        self.id = id
-        self.title = title
-        self.genre = genre
-        self.release_year = release_year
+from flask_sqlalchemy import SQLAlchemy
 
-    def create(self, db_connection):
-        cursor = db_connection.cursor()
-        cursor.execute("INSERT INTO games (title, genre, release_year) VALUES (%s, %s, %s)", 
-                       (self.title, self.genre, self.release_year))
-        db_connection.commit()
-        cursor.close()
+db = SQLAlchemy()
 
-    def update(self, db_connection):
-        cursor = db_connection.cursor()
-        cursor.execute("UPDATE games SET title=%s, genre=%s, release_year=%s WHERE id=%s", 
-                       (self.title, self.genre, self.release_year, self.id))
-        db_connection.commit()
-        cursor.close()
-
-    def delete(self, db_connection):
-        cursor = db_connection.cursor()
-        cursor.execute("DELETE FROM games WHERE id=%s", (self.id,))
-        db_connection.commit()
-        cursor.close()
+class Console(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    manufacturer = db.Column(db.String(100), nullable=False)
+    release_year = db.Column(db.Integer, nullable=False)
